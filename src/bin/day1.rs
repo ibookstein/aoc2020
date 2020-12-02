f38a1aa2d670a028fe41bbd59798d2cbb5331251
Itay Bookstein <ibookstein@gmail.com>
2020-12-03 00:41:16 +0200

    aoc2020: Solve day 1

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,6 +1,29 @@
 use aoc2020::aoc_input::get_input;
+use std::collections::HashSet;
 
 fn main() {
     let input = get_input(1);
-    println!("{}", input);
+    let entries: HashSet<_> = input.lines().map(|n| n.parse::<u64>().unwrap()).collect();
+
+    for entry in entries.iter().copied() {
+        let other = 2020 - entry;
+        if entries.contains(&other) {
+            println!("{} * {} = {}", entry, other, entry * other);
+            break;
+        }
+    }
+
+    'outer: for e1 in entries.iter().copied() {
+        for e2 in entries.iter().copied() {
+            let s = e1 + e2;
+            if s > 2020 {
+                continue;
+            }
+            let e3 = 2020 - s;
+            if entries.contains(&e3) {
+                println!("{} * {} * {} = {}", e1, e2, e3, e1 * e2 * e3);
+                break 'outer;
+            }
+        }
+    }
 }
