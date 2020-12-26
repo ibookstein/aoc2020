0a17d3422acbeb26a5e2f26c2fc43aee716e70aa
Itay Bookstein <ibookstein@gmail.com>
2020-12-26 17:19:26 +0200

    aoc2020: Solve day 19 part 2

--- src/bin/day19.rs
+++ src/bin/day19.rs
@@ -1,11 +1,14 @@
 use aoc2020::aoc_input::get_input;
 use regex::Regex;
+use std::collections::HashMap;
 
 #[derive(Debug, Clone)]
 enum Rule {
     Term(char),
     Or(Vec<Vec<usize>>),
 }
+
+type Rules = HashMap<usize, Rule>;
 
 fn parse_rule(s: &str) -> Rule {
     match s.chars().next().unwrap() {
@@ -25,8 +28,8 @@ fn parse_rule(s: &str) -> Rule {
     }
 }
 
-fn parse_rules(lines: &[&str]) -> Vec<Rule> {
-    let mut rules = Vec::with_capacity(lines.len());
+fn parse_rules(lines: &[&str]) -> Rules {
+    let mut rules = HashMap::new();
     for line in lines {
         let mut split = line.split(": ");
         let idx = split.next().unwrap();
@@ -35,16 +38,13 @@ fn parse_rules(lines: &[&str]) -> Vec<Rule> {
 
         let idx = idx.parse::<usize>().unwrap();
         let rule = parse_rule(rule);
-        rules.push((idx, rule));
+        rules.insert(idx, rule);
     }
-    rules.sort_by_key(|(i, _)| *i);
-    assert_eq!(rules.first().unwrap().0, 0);
-    assert_eq!(rules.last().unwrap().0, rules.len() - 1);
-    rules.into_iter().map(|(_, rule)| rule).collect()
+    rules
 }
 
-fn build_regex_recurse(rules: &[Rule], idx: usize) -> String {
-    match &rules[idx] {
+fn build_regex_recurse(rules: &Rules, idx: usize) -> String {
+    match rules.get(&idx).unwrap() {
         Rule::Term(c) => String::from(*c),
         Rule::Or(or) => {
             let mut clauses = Vec::new();
@@ -60,8 +60,68 @@ fn build_regex_recurse(rules: &[Rule], idx: usize) -> String {
     }
 }
 
-fn build_regex(rules: &[Rule]) -> String {
+fn build_regex(rules: &Rules) -> String {
     format!("^{}$", build_regex_recurse(rules, 0))
+}
+
+fn solve_part1(rules: &Rules, messages: &[&str]) {
+    let regex_str = build_regex(&rules);
+    let regex = Regex::new(&regex_str).unwrap();
+    let count = messages.iter().filter(|m| regex.is_match(*m)).count();
+    dbg!(count);
+}
+
+fn find_adjacent_matches(r: &Regex, s: &str) -> Vec<usize> {
+    let mut remainder = s;
+    let mut res = Vec::new();
+    let mut cur_end = 0usize;
+    while let Some(m) = r.find(remainder) {
+        cur_end += m.end();
+        res.push(cur_end);
+        remainder = &remainder[m.end()..];
+    }
+    res
+}
+
+fn check_message_part2(rule42: &Regex, rule31: &Regex, message: &str) -> bool {
+    let rule42_matches = find_adjacent_matches(rule42, message);
+    if rule42_matches.len() < 2 {
+        return false;
+    }
+
+    // Backtrack
+    for (i, m) in rule42_matches[1..].iter().rev().enumerate() {
+        let remainder = &message[*m..];
+
+        let rule31_matches = find_adjacent_matches(rule31, remainder);
+        if rule31_matches.is_empty() {
+            continue;
+        }
+
+        let rule31_end = *rule31_matches.last().unwrap();
+        if rule31_end != remainder.len() {
+            continue;
+        }
+
+        if rule42_matches.len() - i > rule31_matches.len() {
+            return true;
+        }
+    }
+
+    false
+}
+
+fn solve_part2(rules: &Rules, messages: &[&str]) {
+    let rule42 = format!("^{}", build_regex_recurse(rules, 42));
+    let rule42 = Regex::new(&rule42).unwrap();
+    let rule31 = format!("^{}", build_regex_recurse(rules, 31));
+    let rule31 = Regex::new(&rule31).unwrap();
+
+    let count = messages
+        .iter()
+        .filter(|m| check_message_part2(&rule42, &rule31, m))
+        .count();
+    dbg!(count);
 }
 
 fn main() {
@@ -73,9 +133,6 @@ fn main() {
     let rules = parse_rules(groups[0]);
     let messages = groups[1];
 
-    let regex_str = build_regex(&rules);
-    let regex = Regex::new(&regex_str).unwrap();
-
-    let count = messages.iter().filter(|m| regex.is_match(*m)).count();
-    dbg!(count);
+    solve_part1(&rules, messages);
+    solve_part2(&rules, messages);
 }
