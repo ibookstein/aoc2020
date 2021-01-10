763f29e2bbe9844d4d4fb59a0664672016690214
Itay Bookstein <ibookstein@gmail.com>
2021-01-10 23:39:38 +0200

    aoc2020: Solve day 23 part 2

--- src/bin/day23.rs
+++ src/bin/day23.rs
@@ -1,83 +1,82 @@
 use aoc2020::aoc_input::get_input;
-use std::collections::VecDeque;
-
-type Cup = u32;
-type Cups = VecDeque<Cup>;
 
 #[derive(Debug, Clone)]
 struct Game {
-    cups: Cups,
-    max_label: Cup,
+    cups: Vec<usize>,
+    current: usize
 }
 
 impl Game {
-    fn new(cups: Cups) -> Self {
-        let max_label = cups.len() as Cup;
-        Self { cups, max_label }
+    fn new(cup_labels: &[usize]) -> Self {
+        // Assumes cup_labels contains exactly all integers 1..=cup_labels.len()
+        // Input is 1-based so pad with an extra [0] entry to avoid subtracting
+        // 1 everywhere
+        let mut cups = vec![0; cup_labels.len() + 1];
+        for w in cup_labels.windows(2) {
+            cups[w[0]] = w[1];
+        }
+        cups[*cup_labels.last().unwrap()] = cup_labels[0];
+        Self { cups, current: cup_labels[0] }
     }
 
-    fn next_destination(&self, label: Cup) -> Cup {
+    fn next_destination(&self, label: usize) -> usize {
         if label == 1 {
-            self.max_label
+            (self.cups.len() - 1) as usize
         } else {
             label - 1
         }
     }
 
     fn do_move(&mut self) {
-        let current = self.cups[0];
-        self.cups.rotate_left(1);
-        let pick_up: Vec<_> = self.cups.drain(..3).collect();
+        let current = self.current;
+        let n1 = self.cups[current];
+        let n2 = self.cups[n1];
+        let n3 = self.cups[n2];
 
+        let pickup = [n1, n2, n3];
         let mut destination = self.next_destination(current);
-        let mut destination_pos;
-        loop {
-            destination_pos = self.cups.iter().position(|&e| e == destination as Cup);
-            if destination_pos.is_some() {
-                break;
-            }
+        while pickup.contains(&destination) {
             destination = self.next_destination(destination);
         }
 
-        let insert_pos = destination_pos.unwrap() + 1;
-        for i in 0..pick_up.len() {
-            self.cups.insert(insert_pos + i, pick_up[i]);
-        }
+        let after_destination = self.cups[destination];
+        let after_pickup = self.cups[n3];
+
+        self.current = after_pickup;
+        self.cups[current] = after_pickup;
+
+        self.cups[destination] = n1;
+        self.cups[n3] = after_destination;
     }
 
     fn do_moves(&mut self, count: usize) {
         for _i in 0..count {
             self.do_move();
-
-            if _i % 0x10000 == 0 {
-                dbg!(_i);
-            }
         }
     }
 
     fn labels_after_1(&self) -> String {
-        let pos_after = self.cups.iter().position(|&e| e == 1).unwrap() + 1;
-        let mut s = String::with_capacity(self.cups.len() - 1);
+        let mut s = String::with_capacity(self.cups.len() - 2);
+        let mut cur = self.cups[1];
 
-        for i in pos_after..pos_after + self.cups.len() - 1 {
-            let label = self.cups[i % self.cups.len()];
-            s.push(std::char::from_digit(label as u32, 10).unwrap());
+        while cur != 1 {
+            s.push(std::char::from_digit(cur as u32, 10).unwrap());
+            cur = self.cups[cur];
         }
         s
     }
 
     fn mul_two_labels_after_1(&self) -> usize {
-        let pos = self.cups.iter().position(|&e| e == 1).unwrap();
-        let a = self.cups[(pos + 1) % self.cups.len()];
-        let b = self.cups[(pos + 2) % self.cups.len()];
-        a as usize * b as usize
+        let a = self.cups[1];
+        let b = self.cups[a];
+        a * b
     }
 }
 
-fn parse_cups(input: &str) -> Cups {
+fn parse_cups(input: &str) -> Vec<usize> {
     input
         .chars()
-        .map(|c| c.to_digit(10).unwrap() as Cup)
+        .map(|c| c.to_digit(10).unwrap() as usize)
         .collect()
 }
 
@@ -85,16 +84,16 @@ fn main() {
     let input = get_input(23);
     let orig_cups = parse_cups(input.trim());
 
-    let mut game = Game::new(orig_cups.clone());
+    let mut game = Game::new(orig_cups.as_slice());
     game.do_moves(100);
     dbg!(game.labels_after_1());
 
     let mut orig_cups = orig_cups;
     let start = *orig_cups.iter().max().unwrap() + 1;
-    let range = start..start + 1_000_000 - orig_cups.len() as Cup;
+    let range = start..start + 1_000_000 - orig_cups.len();
     orig_cups.extend(range);
     assert_eq!(orig_cups.len(), 1_000_000);
-    let mut game = Game::new(orig_cups);
+    let mut game = Game::new(orig_cups.as_slice());
     game.do_moves(10_000_000);
     dbg!(game.mul_two_labels_after_1());
 }
@@ -108,7 +107,7 @@ mod tests {
     #[test]
     fn test_labels_after_1() {
         let input = "389125467";
-        let mut game = Game::new(parse_cups(&input));
+        let mut game = Game::new(parse_cups(&input).as_slice());
         game.do_moves(100);
         assert_eq!(game.labels_after_1(), "67384529");
     }
